ea9796b9d22477fb4317fead506a399a7bfbcf87
obalaweb <josephivanobala5@gmail.com>
2020-09-13 17:40:29 +0300

    Updates

---
+++ precache-manifest.5fba52f4db28a1eb40ae21dca81185aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7318a60f5e160601959bec7b53af54fa",
+    "url": "/github.io/React-App.git/index.html"
+  },
+  {
+    "revision": "360bff83a2907f7953b1",
+    "url": "/github.io/React-App.git/static/css/main.a51734db.chunk.css"
+  },
+  {
+    "revision": "551a941e46488566c434",
+    "url": "/github.io/React-App.git/static/js/2.a57ef4bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/React-App.git/static/js/2.a57ef4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360bff83a2907f7953b1",
+    "url": "/github.io/React-App.git/static/js/main.15dc2efb.chunk.js"
+  },
+  {
+    "revision": "1449056244253a991ed5",
+    "url": "/github.io/React-App.git/static/js/runtime-main.ca276f27.js"
+  }
+]);
